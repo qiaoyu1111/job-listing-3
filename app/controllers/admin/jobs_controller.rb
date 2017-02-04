09fa920ba5ec09da548ecefc01a8062130f173ca
qiaoyu <qiaoyu1111358@gmail.com>
2017-02-04 18:38:02 +0800

    implement part 2

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,51 +1,53 @@
 class Admin::JobsController < ApplicationController
-  before_filter :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
-  before_filter :require_is_admin
+    before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+    before_action :require_is_admin
 
-  def show
-    @job = Job.find(params[:id])
-  end
-
-  def index
-    @jobs = Job.new
-  end
-
-  def new
-    @job = Job.new(job_params)
-
-    if @job.save
-      redirect_to admin_jobs_path
-    elserender :new
+    def show
+      @job = Job.find(params[:id])
     end
-  end
 
-  def edit
-    @job = Job.find(params[:id])
-
-    if @job.update(job_params)
-      redirect_to admin_jobs_path
-    else
-      render :edit
-  end
-
-  def destroy
-    @job = Job.find(params[:id])
-
-    @job.destroy
-
-    redirect_to admin_jobs_path
-  end
-
-  def require_is_admin
-    if !current_user.admin?
-      flash[:alert] = 'You are not admin'
-      redirect_to root_path
+    def index
+      @jobs = Job.all
     end
-  end
 
-  private
+    def new
+      @job = Job.new
+    end
 
-  def job_params
-    params.require(:job).permit(:title, :description)
-  end
+    def create
+      @job = Job.new(job_params)
+
+      if @job.save
+        redirect_to admin_jobs_path
+      else
+        render :new
+      end
+    end
+
+    def edit
+      @job = Job.find(params[:id])
+    end
+
+    def update
+      @job = Job.find(params[:id])
+      if @job.update(job_params)
+        redirect_to admin_jobs_path
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @job = Job.find(params[:id])
+
+      @job.destroy
+
+      redirect_to admin_jobs_path
+    end
+
+    private
+
+    def job_params
+      params.require(:job).permit(:title, :description)
+    end
 end
